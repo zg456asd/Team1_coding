a5ce405e5d60fc5b9ac2e1acf6dc4887ff791e27
Teoluo <1026620042@qq.com>
2019-03-19 16:18:24 +0800

    Add files via upload

--- 1st/KDE.R
+++ 1st/KDE.R
@@ -1,19 +1,26 @@
 library(sp) 
 library(spatstat)
 library(maptools)
+
 ## row col values
+## 设置行列的值
 rowValues = 1:5
 colValues<- 1:14
 
 ## draw the polygons
+## 创建多边形对象
 tepList<- c()
 for(rowValue in rowValues){
   for(colValue in colValues){
+    ## 设置每一个多边形的数值
     seatNum <- (rowValue-1)*14+colValue
-    ## paste str and number
+    ## paste str and number   
+    ## 格式化成Sr1、Sr2...
     seatNum = paste("Sr",seatNum,sep="") 
     ## draw island
+    ## 通过polygons函数来画出island
     Sr = Polygon(cbind(c(rowValue,rowValue,rowValue+1,rowValue+1,rowValue),c(colValue,colValue+1,colValue+1,colValue,colValue)))
+    ## 给每一个island标上对应的id
     Srs = Polygons(list(Sr),seatNum)
     ## cun
     tepList<-c(tepList,Srs)
@@ -21,15 +28,19 @@ for(rowValue in rowValues){
 }
 SpP=SpatialPolygons(tepList,70:1)
 
-# from SpatialPolygons  to  “owin”  object (window)
+## from SpatialPolygons  to  “owin”  object (window)
+## 将SpatialPolygons转换成owin对象
 cityOwin <- as.owin(SpP)
 class(cityOwin)
 
 ## add atrr
+## 添加属性值
 ## import the data (seat.csv)
+## 导入seat.csv文件的数据
 mydata1 <- read.table( file = "D:/seat.csv", header = TRUE, sep = "," )
 
 ## select the gpa over 3.3
+## 选定GPA的值
 goodGap<-3.3
 great<-c()
 for (i in 1:length(mydata1[,2])){
@@ -48,35 +59,31 @@ for(value in 1:70){
 df <- data.frame(gpa=1:70,row.names =name )
 
 ## modify the row's name
+
+## 创建空间多边形数据帧数据
 SrDf = SpatialPolygonsDataFrame(SpP,df)
 as(SrDf, "data.frame")
 
-# SpatialPointsDataFrame to coordinates 
+## SpatialPointsDataFrame to coordinates 
+## 把SpatialPointsDataFrame转换成coordinates类型
 pts <- coordinates(SrDf)
-
+## 选出好的部分的坐标数据
 test<-c()
 for( i in great){
   test<-c(test,pts[i,])
 }
-
-rowM<-c()
-colM<-c()
-for( i in 1:length(test)){
-  if(i%%2==1){
-    rowM<-c(rowM,test[i])
-  }else{
-    colM<-c(colM,test[i])
-  }
-}
-
+## 把左边数据放入矩阵
 ptss<-matrix(data = test, nrow = length(test)/2, ncol = 2,byrow=T)
 
-#vreate a 'ppp' object
+## create a 'ppp' object
+## 创建PPP  点模型对象
 p <- ppp(ptss[,1], ptss[,2], window=cityOwin)
 plot(p)
 
-# calculate the density
-# bw * adjust
+## calculate the density
+## bw * adjust
+## 使用密度函数， 并设置带宽
 ds <- density(p, bw ="nrd0",adjust = 1)
-# draw 
+## draw 
+## 画出最终的核密度图
 plot(ds, main='Seat density')
--- 1st/Quadrat-analysis.R
+++ 1st/Quadrat-analysis.R
@@ -1,32 +1,43 @@
 library(sp) 
 
-## row col values
-rowValues = 1:5
-colValues<- 1:14
+## row col values 
+## 设置行列的值 
+rowValues <- 1:5
+colValues <- 1:14
 
-## draw the polygons
+## draw the polygons  
+## 画出多边形格子
 tepList<- c()
 for(rowValue in rowValues){
   for(colValue in colValues){
+    ## set the number of seat  
+    ## 设置每一个多边形的数值
     seatNum <- (rowValue-1)*14+colValue
-    ## paste str and number
+    ## paste str and number   
+    ## 格式化成Sr1、Sr2...
     seatNum = paste("Sr",seatNum,sep="") 
-    ## draw island
+    ## draw island  
+    ## 通过polygons函数来画出island
     Sr = Polygon(cbind(c(rowValue,rowValue,rowValue+1,rowValue+1,rowValue),c(colValue,colValue+1,colValue+1,colValue,colValue)))
+    ## 给每一个island标上对应的id
     Srs = Polygons(list(Sr),seatNum)
-    ## store
+    ## store 将对应好的polygons数据存储在向量中
     tepList<-c(tepList,Srs)
   }
 }
 SpP=SpatialPolygons(tepList,70:1)
 
-## add atrr
-## import the data (seat.csv)
+## add atrr  
+## 添加属性值
+## import the data (seat.csv)   
+## 导入seat.csv文件的数据
 mydata1 <- read.table( file = "D:/seat.csv", header = TRUE, sep = "," )
 
-## select the gpa over 3.3
+## select the gpa over 3.3   
+## 选定GPA的值
 goodGap<-3.3
 
+## 存储大于这个GPA值的座位信息  筛选掉使用缺省值的数据
 great<-c()
 for (i in 1:length(mydata1[,2])){
   if(mydata1[i,2]>=goodGap && !is.na(mydata1[i,3]) ){
@@ -34,7 +45,7 @@ for (i in 1:length(mydata1[,2])){
   }
 }
 
-## select what we need store in a vector
+## select what we need store in a vector  
 name<-c()
 for(value in 1:70){
   seatNum = paste("Sr",value,sep="")
@@ -43,9 +54,10 @@ for(value in 1:70){
 
 ## create a vector include 70 seats
 df <- data.frame(gpa=1:70,row.names =name )
-
 greatCount=0
 ## modify the value >3.3  ==1  <3.3  ==0
+## 大于等于设定值的 值设为1
+## 小于设定值的  值设为0
 for(i in 1:length(df[,1])){
   if(rownames(df)[i] %in% great){
     df[i,1]<-1
@@ -54,28 +66,35 @@ for(i in 1:length(df[,1])){
     df[i,1]<-0
 }
 
+## 创建空间多边形数据帧数据
 ## modify the row's name
 SrDf = SpatialPolygonsDataFrame(SpP,df)
 as(SrDf, "data.frame")
-
+## 绘制网格
 ## draw
 spplot(SrDf)
 
 
 ## select sample and 
-
+## 选择样方  新建数据帧
 df2<-data.frame(row.names <- c(1,2,3,4,5,6),count <- c(4,2,4,2,1,3))
+
 ## count the yangfang 
+## 计算样方数
 n <- length(df2[,1])
+
 ## av is the average of the yangfang
+## 计算平均值
 mean <- greatCount/ n 
-##
+
+## 计算方差
+## variance
 fcSum <- 0
 for( i in 1:length(df2[,1])){
-  fcSum <- fcSum+(df2[i,2]-mean)**2
+  fcSum <- fcSum +(df2[i,2]-mean)**2
 }
-## variance
 variance <- fcSum/(n-1)
+
 ## VMR
 VMR <- variance/mean
 VMR
